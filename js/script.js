1443ccd0c3831e5bcd3c2f0bdbea227d32bbc31e
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-11-03 22:06:45 +0000

    added calculator

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 "use strict"
 
 /**
- * Check servie worker.
+ * Check servcie worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit3-04-JS/sw.js", {
@@ -16,8 +16,15 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function converts fahrenheit to celsius.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function convert() {
+  //input
+  const fahrenheit = parseFloat(document.getElementById("fahrenheit").value)
+
+  //process
+  const celsius = (fahrenheit - 32) * (5.0 / 9.0)
+
+  //output
+  document.getElementById("celsius").innerHTML = `Celsius: ${celsius.toFixed(2} °C`
 }
